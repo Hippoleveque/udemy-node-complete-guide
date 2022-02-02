d28684f5360d1a99fff6fec58c13cfd9ae091db8
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-02-02 23:09:35 +0100

    Finished Section 3

--- vanilla-http-server/app.js
+++ vanilla-http-server/app.js
@@ -1,21 +1,6 @@
 const http = require("http");
+const { handler } = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  res.write("<html>");
-  res.write("<head><title>Enter Message</title></head>");
-  res.write("<body><h1>Welcome to Node JS</h1></body>");
-  res.write("</html>");
-  return res.end();
-});
+const server = http.createServer(handler);
 
 server.listen(3000);
