58dc1f8ee180acf71bce41d3d85fc3948b44d51b
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-02-02 22:21:46 +0100

    First node server

---
+++ vanilla-http-server/app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.write("<html>");
+  res.write("<head><title>Enter Message</title></head>");
+  res.write("<body><h1>Welcome to Node JS</h1></body>");
+  res.write("</html>");
+  return res.end();
+});
+
+server.listen(3000);
