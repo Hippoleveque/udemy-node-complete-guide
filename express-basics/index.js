634e8da66d0b1a9f04177d98ab982f201f673c7c
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-02-05 09:45:11 +0100

    Before templating engines

--- express-basics/index.js
+++ express-basics/index.js
@@ -3,16 +3,20 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const rootDir = require("./helpers/path");
-const adminRoutes = require("./routes/admin");
+const { router: adminRoutes } = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).rebder(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
--- express-basics/routes/shop.js
+++ express-basics/routes/shop.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const { products } = require("./admin");
 
 const rootDir = require("../helpers/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
