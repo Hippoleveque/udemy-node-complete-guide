7d8f5f55afcfba4b474cdbec88f03c1cec029319
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-04-30 16:07:25 +0200

    Added reset token to the user

--- cookies/controllers/auth.js
+++ cookies/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import sgMail from '@sendgrid/mail';
+import crypto from "crypto";
 import 'dotenv/config';
 
 sgMail.setApiKey(process.env.SENDGRID_KEY);
@@ -67,8 +68,8 @@ export const postSignup = async (req, res, next) => {
       to: email,
       from: 'hippolyte.leveque@gmail.com',
       subject: 'Automation test',
-      text: 'Zemmour 2027',
-      html: '<strong>Zemmour Président</strong>',
+      text: 'Reset password',
+      html: '<strong>Reset password</strong>',
     }
     await sgMail.send(msg);
     res.redirect("/login");
@@ -88,18 +89,22 @@ export const getReset = (req, res, next) => {
 
 export const postReset = async (req, res, next) => {
   const { email } = req.body;
+  const buffer = crypto.randomBytes(32);
+  const token = buffer.toString("hex");
   try {
     let user = await User.findOne({ email: email }).exec();
     if (!user) {
       req.flash("error", "Invalid email / password");
       res.redirect("/reset");
     }
+    user.resetPwdToken = token;
+    user.resetPwdTokenExpirationDate = Date.now() + 3600000;
     const msg = {
       to: email,
       from: 'hippolyte.leveque@gmail.com',
       subject: 'Reset password',
-      text: 'Zemmour 2027',
-      html: '<strong> Reseting your password </strong>',
+      text: 'Reset password',
+      html: `Click this <a href="${process.env.HOST_URL}/reset/${token}">link </a> for resetting your password.`
     }
     await sgMail.send(msg);
     req.flash("info", "A link to reset your password has been sent to your inbox.")
--- cookies/models/user.js
+++ cookies/models/user.js
@@ -10,6 +10,8 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  resetPwdToken: String,
+  resetPwdTokenExpirationDate: Date,
   cart: {
     items: [
       {
