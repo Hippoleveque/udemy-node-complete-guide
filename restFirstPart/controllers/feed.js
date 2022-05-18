fd6a4e70d98dbdf192783da16a371c2b86dc2b32
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-05-18 23:00:08 +0200

    Added the route and the controller for updating a post.

--- restFirstPart/controllers/feed.js
+++ restFirstPart/controllers/feed.js
@@ -1,5 +1,11 @@
 import Post from "../models/post.js";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 import { validationResult } from "express-validator";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const getPosts = async (req, res, next) => {
   const posts = await Post.find();
@@ -58,6 +64,57 @@ export const getPost = async (req, res, next) => {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
-    throw err;
+    next(err);
   }
+};
+
+export const updatePost = async (req, res, next) => {
+  const { postId } = req.params;
+  const errors = validationResult(req);
+  const { title, content } = req.body;
+  let imageUrl = req.body.image;
+  try {
+    if (!errors.isEmpty()) {
+      const statusCode = 422;
+      const message = "Something in validation went wrong";
+      const err = new Error(message);
+      err.statusCode = statusCode;
+      throw err;
+    }
+
+    const post = await Post.findById(postId).exec();
+    if (!post) {
+      const err = new Error("Not post was found for this ID");
+      err.statusCode = 404;
+      throw err;
+    }
+    if (req.file) {
+      imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+      const err = new Error("No file picked.");
+      err.statusCode = 422;
+      throw err;
+    }
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl);
+    }
+    post.content = content;
+    post.title = title;
+    post.imageUrl = imageUrl;
+    const result = await post.save();
+    res.status(200).json({ message: "postUpdated", post: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+const clearImage = (imageUrl) => {
+  const imagePath = path.join(__dirname, "..", imageUrl);
+  fs.unlink(imagePath, (err) => {
+    console.log(err);
+  });
 };
