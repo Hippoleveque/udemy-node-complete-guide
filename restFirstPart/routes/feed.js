b0229f20a517393053780e941c51a467f1dd535c
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-05-21 16:38:50 +0200

    Added authentication middleware to all protected routes.

--- restFirstPart/routes/feed.js
+++ restFirstPart/routes/feed.js
@@ -20,10 +20,11 @@ router.post(
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   createPost
 );
 
-router.get("/post/:postId", getPost);
+router.get("/post/:postId", isAuth, getPost);
 
 router.put(
   "/post/:postId",
@@ -31,9 +32,10 @@ router.put(
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   updatePost
 );
 
-router.delete("/post/:postId", deletePost);
+router.delete("/post/:postId", isAuth, deletePost);
 
 export default router;
