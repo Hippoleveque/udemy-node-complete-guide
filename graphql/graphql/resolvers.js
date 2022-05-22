7feacad2d88e9e8362bcbe97dbeca938866b91a1
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-05-22 11:59:08 +0200

    Login logic on the backend.

--- graphql/graphql/resolvers.js
+++ graphql/graphql/resolvers.js
@@ -1,6 +1,8 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const createUser = async ({ inputData }, req) => {
   const { email, name, password } = inputData;
@@ -38,6 +40,53 @@ const createUser = async ({ inputData }, req) => {
   }
 };
 
+const login = async ({ email, password }, req) => {
+  const validationErrs = [];
+  if (!validator.isEmail(email)) {
+    validationErrs.push({ message: "Email is invalid" });
+  }
+  if (
+    validator.isEmpty(password) ||
+    !validator.isLength(password, { min: 5 })
+  ) {
+    validationErrs.push({ message: "Password too short" });
+  }
+  if (validationErrs.length > 0) {
+    const err = new Error("Invalid Input.");
+    err.data = validationErrs;
+    err.code = 422;
+    throw err;
+  }
+  try {
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      const error = new Error("User not found");
+      error.code = 404;
+      throw err;
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+      const err = new Error("Invalid password");
+      err.code = 401;
+      throw err;
+    }
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id.toString(),
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    return { userId: user._id.toString(), token };
+  } catch (err) {
+    if (!err.statusCode) {
+      err.code = 500;
+    }
+    throw err;
+  }
+};
+
 export const root = {
   createUser,
+  login
 };
--- graphql/graphql/schema.js
+++ graphql/graphql/schema.js
@@ -20,6 +20,11 @@ export default buildSchema(`
         posts: [Post!]!
     }
 
+    type AuthData {
+        userId: String!
+        token: String!
+    }
+
     input UserInputData {
         email: String!
         name: String!
@@ -31,7 +36,7 @@ export default buildSchema(`
     }
 
     type RootQuery {
-        hello: String!
+        login(email: String!, password: String!): AuthData!
     }
 
     schema {
