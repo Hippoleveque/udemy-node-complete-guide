65371761666dd16052a22f8bb59b0c7d095b0264
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-05-22 10:57:17 +0200

    Created the first mutation on the backend side for creating user.

--- graphql/graphql/resolvers.js
+++ graphql/graphql/resolvers.js
@@ -1,5 +1,26 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
+const createUser = async ({ inputData }, req) => {
+  const { email, name, password } = inputData;
+  try {
+    const existingUser = await User.findOne({ email }).exec();
+    if (existingUser) {
+      const error = new Error("A user with this mail address already exists");
+      throw error;
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ email, password: hashedPassword, name });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    throw err;
+  }
+};
+
 export const root = {
-  hello: () => {
-    return { text: "Hello world!", views: 1234 };
-  },
+  createUser,
 };
--- graphql/graphql/schema.js
+++ graphql/graphql/schema.js
@@ -1,17 +1,42 @@
 import { buildSchema } from "graphql";
 
 export default buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        updatedAt: String!
+        createdAt: String!
+    }
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(inputData: UserInputData): User
     }
 
     type RootQuery {
-        hello: TestData
+        hello: String!
     }
 
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
 
 `);
