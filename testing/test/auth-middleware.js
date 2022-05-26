2a3efc070028cd33d95cfb8b28724478b20b8673
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-05-26 16:11:49 +0200

    Added a describe header to group auth middleware related tests.

--- testing/test/auth-middleware.js
+++ testing/test/auth-middleware.js
@@ -2,18 +2,20 @@ const expect = require("chai").expect;
 
 const authMiddleware = require("../middleware/is-auth.js");
 
-it("should raise an error when not authenticated", () => {
-  const req = {
-    get: () => null,
-  };
-  expect(authMiddleware.bind(this, req, null, () => {})).to.throw(
-    "Not authenticated."
-  );
-});
+describe("Auth Middleware", () => {
+  it("should raise an error when not authenticated", () => {
+    const req = {
+      get: () => null,
+    };
+    expect(authMiddleware.bind(this, req, null, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
 
-it("should raise an error if the token is not split in two parts", () => {
-  const req = {
-    get: () => "xyz",
-  };
-  expect(authMiddleware.bind(this, req, null, null)).to.throw();
+  it("should raise an error if the token is not split in two parts", () => {
+    const req = {
+      get: () => "xyz",
+    };
+    expect(authMiddleware.bind(this, req, null, null)).to.throw();
+  });
 });
