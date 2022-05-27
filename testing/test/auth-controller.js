296debd2bf19b0d28551370f40c322b60fa73af8
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-05-27 13:51:52 +0200

    Tested user status with test database

--- testing/test/auth-controller.js
+++ testing/test/auth-controller.js
@@ -1,7 +1,8 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
-const { login } = require("../controllers/auth.js");
+const { login, getUserStatus } = require("../controllers/auth.js");
 const User = require("../models/user.js");
 
 describe("Auth Controller - Login", () => {
@@ -20,5 +21,37 @@ describe("Auth Controller - Login", () => {
       done();
     });
     User.findOne.restore();
+  });
+
+  it("should get the correct status code", async () => {
+    await mongoose.connect("mongodb://localhost:27017/test-shop");
+    const user = new User({
+      posts: [],
+      _id: "6289facee3bd1e49c6338892",
+      email: "test@test.com",
+      password: "123",
+      name: "test",
+    });
+    await user.save();
+    const req = {
+      userId: "6289facee3bd1e49c6338892",
+    };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (obj) {
+        console.log("json called");
+        this.userStatus = obj.status;
+      },
+    };
+    const result = await getUserStatus(req, res, () => {});
+    expect(res).to.have.property("statusCode", 200);
+    expect(res).to.have.property("userStatus", "I am new!");
+    await User.deleteMany({});
+    await mongoose.disconnect();
   });
 });
